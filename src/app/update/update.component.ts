8ff6b65ae7b2d0576c682deb66a4f30ba44d801b
Staisha Neville <sjnevill@uwaterloo.ca>
2023-05-08 18:02:58 -0400

    Added submission form in on the update page

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent {
+  constructor(private http: HttpClient, private router: Router){}
 
+  getUserFormData(data: any){
+    let title = data.title;
+    let body = data.body;
+    console.warn(data);
+    this.http.post('https://jsonplaceholder.typicode.com/posts', {title, body})
+    .subscribe((res: any)=>{
+      console.log(this);
+    });
+  }
 }
